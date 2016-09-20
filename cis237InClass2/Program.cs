7cc439228303224d4c319386e61ea418be5a8964
Robert Cooely <Daimyo121@aol.com>
2016-09-20 17:11:56 -0400

    WRite a factorial program

--- cis237InClass2/Program.cs
+++ cis237InClass2/Program.cs
@@ -10,6 +10,18 @@ namespace cis237InClass2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter 1 for factorial or 2 for Tower of Hanoi");
+            string userInput = Console.ReadLine();
+
+            if (userInput == "1") 
+            {
+                //The Dawn way(work done in constructor)
+                Factorial factorial = new Factorial();
+            }
+            else
+            {
+                //DO towers of Hanoi
+            }
         }
     }
 }
